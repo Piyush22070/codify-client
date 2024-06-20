10e1293aeb1585e648c7cd29425a75c571ba5cb9
Piyush22070 <piyushbhoyar6480@gmail.com>
2024-06-20 16:36:30 +0530

    rocket height adjusted

--- src/app/components/Rocket.tsx
+++ src/app/components/Rocket.tsx
@@ -2,7 +2,7 @@
 import Spline from '@splinetool/react-spline/next';
 export default function Home() {
   return (
-    <main className='md:h-[800px]'>
+    <main className=' h-[350px] md:h-[800px]'>
       <Spline
         scene="images/rocket.splinecode" 
       />
